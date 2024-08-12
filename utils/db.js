95293719755001dbcb81958d6ab1690c5cb509d0
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-08-12 14:37:07 +0100

    add database and redis models and other needed functions

---
+++ utils/db.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import Recruiter from '../modules/recruiter.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const user = process.env.USER;
+const pwd = process.env.PASSWORD;
+
+class Database {
+  static async connect () {
+    await mongoose.connect(`mongodb+srv://${user}:${pwd}@cluster0.h7wda.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+    console.log('Database Connected Successfully');
+  }
+
+  static async stats (req, res) {
+    try {
+      const count = await Recruiter.countDocuments({});
+      res.status(200).json({ Recruiters: count, Developers: 0 });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+}
+
+export default Database;
