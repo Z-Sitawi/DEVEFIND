a347b60cf8cb3603cdb94066f7fe038eff7923ae
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-08-13 14:19:10 +0100

    add developer schema module

---
+++ modules/developer.js
@@ -0,0 +1,158 @@
+import mongoose from 'mongoose';
+
+const DeveloperSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    lowercase: true
+  },
+  lastname: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    lowercase: true
+  },
+  age: {
+    type: Number,
+    required: false
+  },
+  gender: {
+    type: String,
+    required: false,
+  },
+  country: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: {
+      validator: function (v) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid email!`
+    }
+  },
+  backupEmail: {
+    type: String,
+    required: false,
+    trim: true,
+    lowercase: true,
+    default: "",
+    validate: {
+      validator: function (v) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid email!`
+    }
+  },
+  phone: {
+    type: String,
+    required: false,
+    trim: true,
+    validate: {
+      validator: function (v) {
+          return /^\+\d{9,15}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  languages: [{
+    language: {type: String},
+    proficiency: {type: String}
+  }],
+  image: {
+    type: String,
+    default: '../static/user.png'
+  },
+  summary: {
+    headline: {type: String, maxlength: 100},
+    description: {type: String, maxlength: 700}
+  },
+  education: [{
+    institution: {type: String, maxlength: 100},
+    degree: {type: String, maxlength: 100},
+    fieldOfStudy: {type: String, maxlength: 100},
+    graduationDate: {type: Date}
+  }],
+  experience: [{
+    jobTitle: {type: String, maxlength: 100},
+    companyName: {type: String, maxlength: 100},
+    location: {type: String, maxlength: 100},
+    startDate: {type: Date},
+    endDate: {type: Date},
+    responsibilities: [{type: String, maxlength: 500}]
+  }],
+  portfolio: [{
+    projectName: {type: String, maxlength: 100},
+    technologiesUsed: [{type: String}],
+    role: {type: String, maxlength: 250},
+    description: {type: String, maxlength: 500},
+    link: {type: String, validate: {
+      validator: function (v) {
+        return /^(https?:\/\/[^\s$.?#].[^\s]*)$/.test(v);
+      },
+      message: props => `${props.value} is not a valid URL!`
+    }}
+  }],
+  links: {
+    gitHub: {type: String, validate: {
+      validator: function (v) {
+        return /^(https?:\/\/github\.com\/[^\s$.?#].[^\s]*)$/.test(v);
+      },
+      message: props => `${props.value} is not a valid GitHub URL!`
+    }},
+    linkedin: {type: String, validate: {
+      validator: function (v) {
+        return /^(https?:\/\/www\.linkedin\.com\/in\/[^\s$.?#].[^\s]*)$/.test(v);
+      },
+      message: props => `${props.value} is not a valid LinkedIn URL!`
+    }},
+    facebook: {type: String, validate: {
+      validator: function (v) {
+        return /^(https?:\/\/www\.facebook\.com\/[^\s$.?#].[^\s]*)$/.test(v);
+      },
+      message: props => `${props.value} is not a valid Facebook URL!`
+    }},
+    blog: {type: String, validate: {
+      validator: function (v) {
+        return /^(https?:\/\/[^\s$.?#].[^\s]*)$/.test(v);
+      },
+      message: props => `${props.value} is not a valid blog URL!`
+    }},
+    personalWebsite: {type: String, validate: {
+      validator: function (v) {
+        return /^(https?:\/\/[^\s$.?#].[^\s]*)$/.test(v);
+      },
+      message: props => `${props.value} is not a valid personal website URL!`
+    }}
+  },
+  jobTitle: {
+    type: String,
+    required: false
+  },
+  certifications: [{
+    name: {type: String, maxlength: 100},
+    issuingOrganization: {type: String, maxlength: 100},
+    dateIssued: {type: Date}
+  }]
+},
+{
+  timestamps: true
+});
+
+const Developer = mongoose.model('Developer', DeveloperSchema);
+
+export default Developer;
